843d2b9c6f61af0ff683638a485a879acc0e8baf
samsonajulor <samson.ajulor@decagon.dev>
2022-02-10 21:32:37 +0100

    bug-debugged error

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,44 @@
-import createError, { HttpError } from "http-errors";
-import express, { Request, Response, NextFunction } from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import dotenv from "dotenv";
+import createError from 'http-errors'
+import express, {Request, Response, NextFunction} from 'express';
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
-
-import { testConnection } from './db-config/postgres-connection'
+import indexRouter from './routes/index'
+import { testConnection } from './db-config/postgres-connection';
 
 testConnection()
-
-dotenv.config();
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req: Request, res: Response, next: NextFunction) {
   next(createError(404));
 });
 
+
+
 // error handler
-app.use(function (
-  err: HttpError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
-export default app;
+module.exports = app;
