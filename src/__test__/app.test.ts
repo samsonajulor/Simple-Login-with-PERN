3ab1d40b6acd5c275e1615c1e0553f9dec7899f0
samsonajulor <samson.ajulor@decagon.dev>
2022-02-11 10:58:01 +0100

    edited the documentation

---
+++ src/__test__/app.test.ts
@@ -0,0 +1,30 @@
+import supertest from 'supertest'
+const app= require('../app')
+
+let token: string
+
+describe("Auth", () => {
+ const data = {
+   firstname: 'Sam',
+   lastname: 'Aju',
+   email: 'samson12578j@gmail.com',
+   password: '123456',
+ }
+ test("signup", async () => {
+  const response = await supertest(app).post("/users/signup").send(data);
+  expect(response.status).toBe(500);
+  expect(response.body.message).toBe("duplicate key value violates unique constraint \"users_email_key\"");
+  expect(response.body.userDetails.email).toBe(data.email);
+ });
+//  test("login", async () => {
+//   const response = await supertest(app)
+//    .post("/login")
+//    .send({ email: data.email, password: data.password });
+//   token = response.body.user.token
+//   expect(response.status).toBe(201);
+//   expect(response.body.message).toBe("login successful");
+//  });
+});
+//let res:string;
+
+
